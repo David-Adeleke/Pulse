6495d8c4ce52beade5456f8f3d69673f904616ee
David-Adeleke <damoshey08@gmail.com>
2026-04-18 17:39:59 +0100

    style: added the logo and the navbar styles

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 import { restClient } from '@massive.com/client-js';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 import { useEffect, useMemo, useState } from 'react';
 import "../styles/dashboard.css"
 
@@ -46,6 +46,30 @@ function Home() {
 
   return (
     <main className="dashboard-container">
+      <div className="logo-navbar">
+        <div className="logo">
+          <Link to='/'>
+            <h1>PULSENG</h1>
+          </Link>
+        </div>
+
+        <div className="navbar-container">
+            <ul className="navbar">
+              <Link to='/'>
+                <li>Home</li>
+              </Link>
+              <Link to='/portfolios'>
+                <li>Portfolios</li>
+              </Link>
+              <Link to='/trends'>
+                <li>Market Trends</li>
+              </Link>
+              <Link to='/watchlist'>
+                <li>Watchlist</li>
+              </Link>
+            </ul>
+        </div>
+      </div>
       {NGX_TICKERS.map((ticker) => (
         <div key={ticker.symbol} className='ticker-container'>
           <h1 className='ticker-symbol'>{ticker.symbol}</h1>
